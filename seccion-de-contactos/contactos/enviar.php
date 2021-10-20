aa7470ff5b1e4d2309f2871e96dd5ef9538fb646
Edwin Alexander <814892@clases.edu.sv>
2021-10-20 14:19:53 -0600

    agregando al proyecto la secion de contactos

---
+++ seccion-de-contactos/contactos/enviar.php
@@ -0,0 +1,45 @@
+<?php
+$nombre = $_POST["nombre"];
+$email = $_POST["email"];
+$asunto = $_POST["asunto"];
+$mensaje = $_POST["mensaje"];
+
+$body = "Nombre: " . $nombre . "<br>Email: " . $email . "<br>Asunto: " . $asunto . "<br>Mensaje: " . $mensaje;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'facerealtime25@gmail.com';                     //SMTP username
+    $mail->Password   = 'faCe5rEaltiMe"';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('facerealtime25@gmail.com', $nombre);
+    $mail->addAddress('facerealtime25@gmail.com');     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Contacto desde nuestra web';
+    $mail->Body    = $body;
+    $mail->CharSet = 'UTF-8';
+    $mail->send();
+    echo '<script>
+        alert("El mensaje se envió correctamnete");
+        window.history.go(-1);
+        </script>';
+} catch (Exception $e) {
+    echo "Error: {$mail->ErrorInfo}";
+}
