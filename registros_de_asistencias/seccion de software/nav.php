7c433f3552befbaa9a5a54c4b372388119a1d239
Edwin Alexander <814892@clases.edu.sv>
2021-10-25 11:20:49 -0600

    subiendo imagen del logo

--- registros_de_asistencias/seccion de software/nav.php
+++ registros_de_asistencias/seccion de software/nav.php
@@ -1,35 +1,9 @@
 <?php
-/*
-
-  ____          _____               _ _           _       
- |  _ \        |  __ \             (_) |         | |      
- | |_) |_   _  | |__) |_ _ _ __ _____| |__  _   _| |_ ___ 
- |  _ <| | | | |  ___/ _` | '__|_  / | '_ \| | | | __/ _ \
- | |_) | |_| | | |  | (_| | |   / /| | |_) | |_| | ||  __/
- |____/ \__, | |_|   \__,_|_|  /___|_|_.__/ \__, |\__\___|
-         __/ |                               __/ |        
-        |___/                               |___/         
-    
-____________________________________
-/ Si necesitas ayuda, contáctame en \
-\ https://parzibyte.me               /
- ------------------------------------
-        \   ^__^
-         \  (oo)\_______
-            (__)\       )\/\
-                ||----w |
-                ||     ||
-Creado por Parzibyte (https://parzibyte.me).
-------------------------------------------------------------------------------------------------
-Si el código es útil para ti, puedes agradecerme siguiéndome: https://parzibyte.me/blog/sigueme/
-Y compartiendo mi blog con tus amigos
-También tengo canal de YouTube: https://www.youtube.com/channel/UCroP4BTWjfM0CkGB6AFUoBg?sub_confirmation=1
-------------------------------------------------------------------------------------------------
-*/ ?>
+?>
 <nav class="navbar navbar-expand-md navbar-dark bg-success fixed-top">
-    <!-- <a class="navbar-brand" href="https://parzibyte.me/blog">
-        <img class="img-fluid" style="max-width: 200px" src="img/parzibyte_logo.png" loading="lazy">
-    </a> -->
+    <a class="navbar-brand" href="https://parzibyte.me/blog">
+        <img class="img-fluid" style="max-width: 60px" src="img/face_realtime.jpg" loading="lazy">
+    </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" id="botonMenu" aria-label="Mostrar u ocultar menú">
         <span class="navbar-toggler-icon"></span>
     </button>
